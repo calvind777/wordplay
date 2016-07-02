0e499e3d03f1126e107880673c04a647009ad844
Calvin Dong <calvind777@gmail.com>
2016-07-01 17:40:49 -0700

    adds youtube integration and redirect

--- search/forms.py
+++ search/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SongForm(forms.Form):
-    songname = forms.CharField(label = 'word', max_length=100)
+    songtopic = forms.CharField(label = 'word', max_length=100)
